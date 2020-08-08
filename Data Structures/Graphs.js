c5ffb2db6bf4e28e069c7403cc033424f1991185
github username <therealgracechoi@gmail.com>
2020-08-08 16:27:22 -0700

    breadth first traversal function aded

--- Data Structures/Graphs.js
+++ Data Structures/Graphs.js
@@ -37,10 +37,38 @@ g.addBidirectionalEdge("Oregon", "California");
 g.addBidirectionalEdge("Oregon", "Nevada");
 g.addBidirectionalEdge("California", "Nevada");
 
-console.log(g.getNeighbors("Washington"));
-console.log(g.getNeighbors("Oregon"));
-console.log(g.getNeighbors("Idaho"));
-console.log(g.getNeighbors("California"));
-console.log(g.getNeighbors("Nevada"));
+// console.log(g.getNeighbors("Washington"));
+// console.log(g.getNeighbors("Oregon"));
+// console.log(g.getNeighbors("Idaho"));
+// console.log(g.getNeighbors("California"));
+// console.log(g.getNeighbors("Nevada"));
 
 
+
+// GRAPH TRAVERSAL:
+
+// Breadth First Traversal:
+function breadthFirstTraversal() {
+    // pick a node to start with and add to queue: 
+    let q = ["Washington"];
+    // keep track of nodes we've visited:
+    let visited = {}
+
+    // dequeue one node at a time, 
+    while (q.length > 0) {
+        let node = q.shift();
+        console.log(node);
+        
+        // mark it as visited,
+        visited[node] = true;
+        // get it's neighbors and add them to queue, 
+        // only adding nodes that haven't been visited!
+        for (let neighbor of g.getNeighbors(node)) {
+            if(!visited[neighbor]) {
+                q.push(neighbor);
+                // mark it as visited here again to prevent from adding to queue twice
+                visited[neighbor] = true;
+            }
+        }
+    }
+}
