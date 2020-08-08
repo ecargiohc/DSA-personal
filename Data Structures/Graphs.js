9a35a53df20d28d4ee8353b5cb1640c7f1bcbaac
github username <therealgracechoi@gmail.com>
2020-08-08 16:47:55 -0700

    Depth First Traversal

--- Data Structures/Graphs.js
+++ Data Structures/Graphs.js
@@ -35,19 +35,19 @@ g.addBidirectionalEdge("Washington", "Idaho");
 g.addBidirectionalEdge("Oregon", "Idaho");
 g.addBidirectionalEdge("Oregon", "California");
 g.addBidirectionalEdge("Oregon", "Nevada");
+g.addBidirectionalEdge("Idaho", "Nevada");
 g.addBidirectionalEdge("California", "Nevada");
 
-// console.log(g.getNeighbors("Washington"));
-// console.log(g.getNeighbors("Oregon"));
-// console.log(g.getNeighbors("Idaho"));
-// console.log(g.getNeighbors("California"));
-// console.log(g.getNeighbors("Nevada"));
-
-
+console.log("WA",g.getNeighbors("Washington"));
+console.log("OR", g.getNeighbors("Oregon"));
+console.log("ID", g.getNeighbors("Idaho"));
+console.log("CA", g.getNeighbors("California"));
+console.log("NV", g.getNeighbors("Nevada"));
 
 // GRAPH TRAVERSAL:
 
-// Breadth First Traversal:
+// Breadth First Traversal: visiting the things closer first
+// adding things one at a time into a QUEUE and keeping track of what we 'visited'
 function breadthFirstTraversal() {
     // pick a node to start with and add to queue: 
     let q = ["Washington"];
@@ -72,3 +72,32 @@ function breadthFirstTraversal() {
         }
     }
 }
+console.log(breadthFirstTraversal());
+
+// Depth First Traversal: visiting the things farthest and working back up to closest
+// use of STACK
+function depthFirstTraversal() {
+    // pick a node to start with and add to queue: 
+    let q = ["Washington"];
+    // keep track of nodes we've visited:
+    let visited = {}
+
+    // dequeue one node at a time, 
+    while (q.length > 0) {
+        let node = q.pop();
+        console.log(node);
+        
+        // mark it as visited,
+        visited[node] = true;
+        // get it's neighbors and add them to queue, 
+        // only adding nodes that haven't been visited!
+        for (let neighbor of g.getNeighbors(node)) {
+            if(!visited[neighbor]) {
+                q.push(neighbor);
+                // mark it as visited here again to prevent from adding to queue twice
+                visited[neighbor] = true;
+            }
+        }
+    }
+}
+console.log(depthFirstTraversal());
